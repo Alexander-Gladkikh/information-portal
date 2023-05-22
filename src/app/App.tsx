9c50e3d6be5a103fe7492378c27c24fc18b2128e
Alexander <som17son25@gmail.com>
2023-05-22 10:37:48 +0300

    add Modal component and include eslint-plugin-react-hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,29 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
     const { theme } = useTheme();
+
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                {/* eslint-disable-next-line i18next/no-literal-string,react/button-has-type */}
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    {/* eslint-disable-next-line i18next/no-literal-string */}
+                    {/* eslint-disable-next-line max-len */}
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis minima, nisi perspiciatis quisquam repellendus sed. Autem eligendi fugiat totam?
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
