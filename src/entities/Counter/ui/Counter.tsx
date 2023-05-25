f3f6a2d66e33256dc132bbd9c1d1c468e5fd5dbb
Alexander <som17son25@gmail.com>
2023-05-25 10:53:50 +0300

    install redux toolkit create component Counter

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,35 @@
+import { Button } from 'shared/ui/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { useTranslation } from 'react-i18next';
+import { counterActions } from '../model/slice/counterSlice';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+
+    const counterValue = useSelector((state:StateSchema) => state.counter.value);
+
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+
+    const { t } = useTranslation();
+
+    return (
+        <div>
+            <h1>
+                {conterValue}
+            </h1>
+            <Button onClick={increment}>
+                {t('incremetnt')}
+            </Button>
+            <Button onClick={decrement}>
+                {t('decrement')}
+            </Button>
+        </div>
+    );
+};
