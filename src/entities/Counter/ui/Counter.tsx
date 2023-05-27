558d8870048544e880b9f567144da83cab582ed1
Alexander <som17son25@gmail.com>
2023-05-27 15:50:48 +0300

    create test reducer and Counter component

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -21,13 +21,19 @@ export const Counter = () => {
 
     return (
         <div>
-            <h1>
+            <h1 data-testid="value-title">
                 {counterValue}
             </h1>
-            <Button onClick={increment}>
+            <Button
+                onClick={increment}
+                data-testid="increment-btn"
+            >
                 {t('incremetnt')}
             </Button>
-            <Button onClick={decrement}>
+            <Button
+                onClick={decrement}
+                data-testid="decrement-btn"
+            >
                 {t('decrement')}
             </Button>
         </div>
