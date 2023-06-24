9a9bcc761ab04289eca0ca1e997053cf3da0a41c
Alexander <som17son25@gmail.com>
2023-06-24 22:52:35 +0300

    added global __PROJECT__, unit tests profile, selectors and services

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
+import avatar from 'shared/assets/tests/storybook.jpeg';
+import { ProfileCard } from './ProfileCard';
+
+export default {
+    title: 'entities/ProfileCard',
+    component: ProfileCard,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ProfileCard>;
+
+const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    data: {
+        username: 'admin',
+        age: 29,
+        country: Country.Russia,
+        lastname: 'ulbi',
+        first: 'asd',
+        city: 'asf',
+        currency: Currency.RUB,
+        avatar,
+    },
+};
+
+export const withError = Template.bind({});
+withError.args = {
+    error: 'true',
+};
+
+export const isLoading = Template.bind({});
+isLoading.args = {
+    isLoading: true,
+};
