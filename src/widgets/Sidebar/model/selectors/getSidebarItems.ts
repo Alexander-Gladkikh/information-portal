73c9080aeded4a0957db2c04b94dc6240bc55157
Alexander <som17son25@gmail.com>
2023-07-02 16:16:15 +0300

    fix sidebar

---
+++ src/widgets/Sidebar/model/selectors/getSidebarItems.ts
@@ -0,0 +1,44 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { getUserAuthData } from 'entities/User';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
+import AboutIcon from 'shared/assets/icons/about-20-20.svg';
+import ProfileIcon from 'shared/assets/icons/profile.svg';
+import ArticleIcon from 'shared/assets/icons/article.svg';
+import { SideBarItemType } from '../../model/types/sidebar';
+
+export const getSidebarItems = createSelector(
+    getUserAuthData,
+    (userData) => {
+        const sideBarItemsList: SideBarItemType[] = [
+            {
+                path: RoutePath.main,
+                Icon: MainIcon,
+                text: 'Главная',
+            },
+            {
+                path: RoutePath.about,
+                Icon: AboutIcon,
+                text: 'о сайте ',
+            },
+        ];
+
+        if (userData) {
+            sideBarItemsList.push(
+                {
+                    path: RoutePath.profile + userData.id,
+                    Icon: ProfileIcon,
+                    text: 'Профиль',
+                    authOnly: true,
+                },
+                {
+                    path: RoutePath.articles,
+                    Icon: ArticleIcon,
+                    text: 'Статьи',
+                    authOnly: true,
+                },
+            );
+        }
+        return sideBarItemsList;
+    },
+);
--- src/widgets/Sidebar/ui/SideBarItem/SideBarItem.tsx
+++ src/widgets/Sidebar/ui/SideBarItem/SideBarItem.tsx
@@ -4,8 +4,8 @@ import { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useSelector } from 'react-redux';
 import { getUserAuthData } from 'entities/User';
+import { SideBarItemType } from 'widgets/Sidebar/model/types/sidebar';
 import cls from './SideBarItem.module.scss';
-import { SideBarItemType } from '../../model/items';
 
 interface SideBarItemProps {
    item: SideBarItemType;
