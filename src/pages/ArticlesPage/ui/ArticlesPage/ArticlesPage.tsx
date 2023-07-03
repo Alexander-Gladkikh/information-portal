8baa9265cdebc1e03cc655b46594a04f4e1a7306
Alexander <som17son25@gmail.com>
2023-07-03 14:29:48 +0300

    add article-list, article-item, skeleton and storybook

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,5 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { memo } from 'react';
+import { ArticleList, ArticleView } from 'entities/Article';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
@@ -10,7 +11,20 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     const { className } = props;
 
     return (
-        <div className={classNames(cls.ArticlesPage, {}, [className])} />
+        <div className={classNames(cls.ArticlesPage, {}, [className])}>
+            <ArticleList
+                isLoading
+                view={ArticleView.BIG}
+                articles={
+                    new Array(16)
+                        .fill(0)
+                        .map((item, index) => ({
+                            ...article,
+                            id: String(index),
+                        }))
+                }
+            />
+        </div>
     );
 };
 
