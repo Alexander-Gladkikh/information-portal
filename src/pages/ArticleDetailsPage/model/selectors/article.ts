6bfd48c5c6dff03d985049297559971fe5e7c1a3
Alexander <som17son25@gmail.com>
2023-07-15 20:16:57 +0300

    add create, edit page routes

---
+++ src/pages/ArticleDetailsPage/model/selectors/article.ts
@@ -0,0 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { getArticleDetailsData } from 'entities/Article';
+import { getUserAuthData } from 'entities/User';
+
+export const getCanEditArticle = createSelector(
+    getArticleDetailsData,
+    getUserAuthData,
+    // eslint-disable-next-line consistent-return
+    (article, user) => {
+        if (!article || !user) {
+            return false;
+        }
+        return article.user.id === user.id;
+    },
+);
