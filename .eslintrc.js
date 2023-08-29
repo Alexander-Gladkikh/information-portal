0b04f91ed8374b4870cf6a0760b809289f80c053
Alexander <som17son25@gmail.com>
2023-08-29 19:54:40 +0300

    add linter for layer imports, big refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -74,6 +74,13 @@ module.exports = {
         'no-undef': 'off',
         'no-react/no-array-index-key': 'off',
         'gladkikh-plugin/path-checker': ['error', { alias: '@' }],
+        'gladkikh-plugin/layer-imports': [
+            'error',
+            {
+                alias: '@',
+                ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
+            },
+        ],
         'gladkikh-plugin/public-api-imports': [
             'error',
             {
--- src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
+++ src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line gladkikh-plugin/layer-imports
 import '@/app/styles/index.scss';
 import { Story } from '@storybook/react';
 
